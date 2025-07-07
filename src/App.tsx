eab900a7b6c1bf500ba4342cd870da0530ad8971
Tim Poliquin <tim.poliquin@gmail.com>
2025-07-07 11:41:08 -0400

    Layout improvements and linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import * as React from "react";
+import * as React from 'react';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
-import { SectionCoursework } from './sections/section-coursework/SectionCoursework';
-import { SectionIntro } from './sections/section-intro/SectionIntro';
-import { SectionProjects } from './sections/section-projects/SectionProjects';
-import { SectionPrototypes } from './sections/section-prototypes/SectionPrototypes';
-import AppTheme from "./theme/AppTheme";
+import {SectionCoursework} from './sections/section-coursework/SectionCoursework';
+import {SectionIntro} from './sections/section-intro/SectionIntro';
+import {SectionProjects} from './sections/section-projects/SectionProjects';
+import {SectionPrototypes} from './sections/section-prototypes/SectionPrototypes';
+import AppTheme from './theme/AppTheme';
 
-function App(props: { disableCustomTheme?: boolean }) {
+function App(props: {disableCustomTheme?: boolean}) {
   return (
     <AppTheme {...props}>
       <CssBaseline enableColorScheme />
       <Container
         component="main"
         maxWidth="xl"
-        sx={{paddingTop: 2, paddingBottom: 2, paddingLeft: 0, paddingRight: 0 }}
+        sx={{paddingTop: 2, paddingBottom: 2, paddingLeft: 0, paddingRight: 0}}
       >
-          <SectionIntro />
-          <SectionProjects />
-          <SectionPrototypes />
-          <SectionCoursework />
-
+        <SectionIntro />
+        <SectionProjects />
+        <SectionPrototypes />
+        <SectionCoursework />
       </Container>
     </AppTheme>
   );
--- src/sections/section-coursework/SectionCoursework.tsx
+++ src/sections/section-coursework/SectionCoursework.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import {Typography} from "@mui/material";
-import Grid from "@mui/material/Grid";
+import React from 'react';
+import {Typography} from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 export const SectionCoursework = () => (
-    <Grid container size={12}>
-        <Typography typography="h2">Coursework</Typography>
-    </Grid>
-)
+  <Grid container size={12}>
+    <Typography typography="h2">Coursework</Typography>
+  </Grid>
+);
